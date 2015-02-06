b3d24f7a238c0fa497b1988991ab051cbe9dc7a0
hanqiao69 <hanqiao69@gmail.com>
2015-02-05 20:13:01 -0500

    delete variable rotation direction

--- OrreryGears/src/Gear.java
+++ OrreryGears/src/Gear.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 public class Gear {
 	
 	String name;
-	int size;
+	int radius;
 	double teethNum;
 	double rotateNum;
 	double angularSpeed;
-	boolean direction;
 	boolean isMoved;
 	ArrayList<Gear> sisters = new ArrayList<Gear>();
 	ArrayList<Gear> children = new ArrayList<Gear>();
@@ -35,15 +34,13 @@ public class Gear {
 	
 	public static void rotate (Gear gear1, Gear gear2) {
 		if (isSisters(gear1, gear2)) {
-			gear2.direction = gear1.direction;
 			gear2.rotateNum = (double) (gear1.teethNum / gear2.teethNum) * gear1.rotateNum;
 			gear2.angularSpeed = gear1.angularSpeed;
 		}
 		
 		else if (isParent(gear1, gear2)) {
-			gear2.direction = !gear1.direction;
 			gear2.rotateNum = gear1.rotateNum;
-			gear2.angularSpeed = (double) (gear1.teethNum / gear2.teethNum) * gear1.angularSpeed;
+			gear2.angularSpeed = - (double) (gear1.teethNum / gear2.teethNum) * gear1.angularSpeed;
 		}
 		
 	}
