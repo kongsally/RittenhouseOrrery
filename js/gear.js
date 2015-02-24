c71ba3ceef5af3d199656e7e02099cbb73045b8e
Sally Kong <kongsally94@gmail.com>
2015-02-24 16:06:34 -0500

    merged with rachel's

--- js/gear.js
+++ js/gear.js
@@ -40,10 +40,10 @@ function addToChildren (root, child) {
 
 function rotate (gear1, gear2) {
 	if (isSisters(gear1, gear2)) {
-			gear2.rotateNum = (gear2.teethNum / gear1.teethNum) * gear1.rotateNum;
+			gear2.rotateNum = ((gear2.teethNum / gear1.teethNum) * gear1.rotateNum) % 360;
 		}
 		else if (isParent(gear1, gear2)) {
-			gear2.rotateNum = - gear1.rotateNum;
+			gear2.rotateNum = -(gear1.rotateNum % 360);
 		}
 }
 
@@ -63,7 +63,7 @@ function childrenInteract (root) {
 }
 
 function interact (root, rotateNum) {
-	root.rotateNum = rotateNum;
+	root.rotateNum += rotateNum;
 	sisterInteract(root);
 }
 
