6c7e6c9ac4c80e57131a4054ffe49974f12110a2
Sally Kong <kongsally94@gmail.com>
2015-05-06 19:13:09 -0400

    merged with Karens

--- js/historyEdit.js
+++ js/historyEdit.js
@@ -2,6 +2,68 @@
 var paragraphs = [];
 var editing = false;
 var historyPs = [];
+var signinToggle = false;
+var isAdmin = false;
+
+//toggle sign-in with esc key
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) {
+  	if(!isAdmin) {
+	  	if(!signinToggle) {
+	  		$("#signin").css("display", "block");
+	  	} else {
+	  		$("#signin").css("display", "none");
+	  	}
+  	} else {
+  		if(!signinToggle) {
+  			$("#signout").css("display", "block");
+  		} else {
+  			$("#signout").css("display", "none");
+  		}
+  	}
+  	signinToggle = !signinToggle;
+ }
+});
+
+//initialize Parse
+$(function(){
+	Parse.initialize("vlxm5ebk6uC1dGyTpxHCdUFeUruXyzzMupZq28eB", "YW7U8uMUzlMspFam3lrAdbRYHCVnXuBRHAB4YnSM");
+});
+
+
+function login() {
+	Parse.User.logIn($("#username").val(), $("#password").val(), {
+	  success: function(user) {
+	    // Do stuff after successful login.
+	    var currentUser = Parse.User.current();
+			if (currentUser) {
+		    	// do stuff with the user
+				$("#username").val("");
+				$("#password").val("");
+				$("#signin").css("display","none");
+				$("#signout").css("display", "block");
+				if(currentUser.attributes.username === "admin") {
+					isAdmin = true;
+					$("#editDescription").css("display", "block");
+				}
+			
+			} else {
+		    	// show the signup or login page
+			}
+	  },
+	  error: function(user, error) {
+	    // The login failed. Check error to see why.
+	    alert("Error: " + error.code + " " + error.message);
+	  }
+	});
+}
+
+function signout() {
+	Parse.User.logOut();
+	isAdmin = false;
+	$("#signout").css("display","none");
+	$("#editDescription").css("display", "none");
+}
 
 function editDescription() {
 
